df123385bc12fd4b50cbcd8aee45bca3fadc2f9a
Raheel Arfeen <raheelarfeen@gmail.com>
2025-07-03 19:46:37 +0600

    finished

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 3000;
 
 // Middlewares
 app.use(cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173"],
     credentials: true,
 }));
 app.use(express.json());
@@ -796,7 +796,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
